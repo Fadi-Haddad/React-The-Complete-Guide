1808775ba9b7f7818fa5486d22d25ccad3a08ca8
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-06-26 21:53:20 +0300

    Add Expenseitem.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/Expenseitem";
+// when importing components, we don't include the extension (.js)
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem> 
+       {/* components should start with a capital letter */}
     </div>
   );
 }
